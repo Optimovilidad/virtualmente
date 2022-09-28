1ed52c224eadbe0dadf5301c66f53be60dda3d05
Pierre Dolce <48536105+Petercadeau@users.noreply.github.com>
2022-09-28 16:38:10 -0500

    fix/ corregir error de path de archivo

--- restartRadio.py
+++ restartRadio.py
@@ -1,6 +1,7 @@
 import os
 
-txt_path = os.getcwd() + '/pyt.txt'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+txt_path = dir_path + '/pyt.txt'
 
 os.system('pgrep python3 > '+txt_path)
 archivo = open(txt_path)
@@ -9,5 +10,5 @@ linea = archivo.readlines()
 if(len(linea) > 1):
     for i in range(0, len(linea)-1):
         os.system('kill -9 '+linea[i])
-radio_path = os.getcwd() + '/testR.py'
+radio_path = dir_path + '/testR.py'
 os.system('python3 '+radio_path)
